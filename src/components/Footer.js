23e0c215321ee053127d9ba512a615da6a8b19ec
jd479800 <jd479800@gmail.com>
2023-05-21 20:25:58 -0400

    Adds spacing with icons in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,19 @@ import React from "react";
 import { FaGithubSquare, FaLinkedin, FaFacebookSquare } from "react-icons/fa";
 
 export default function Footer() {
+    const footerStyle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      };
+      const iconStyle = {
+        margin: "10px",
+      };
   return (
-    <div>
-      <a href="https://github.com/jdeshaies"><FaGithubSquare size={40}/></a>
-      <a href="https://www.linkedin.com/in/joseph-deshaies-42b554130"><FaLinkedin size={40}/></a>
-      <a href="https://www.facebook.com/joey.deshaies"><FaFacebookSquare size={40}/></a>
+    <div style={footerStyle}>
+      <a href="https://github.com/jdeshaies"><FaGithubSquare style={iconStyle} size={60}/></a>
+      <a href="https://www.linkedin.com/in/joseph-deshaies-42b554130"><FaLinkedin style={iconStyle} size={60}/></a>
+      <a href="https://www.facebook.com/joey.deshaies"><FaFacebookSquare style={iconStyle} size={60}/></a>
     </div>
   );
 }
